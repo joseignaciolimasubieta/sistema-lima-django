c7c26f23847d5f39ecf11c4e599ebb00e3d13c76
joseignaciolimasubieta_ <joseignaciolimasubieta41@gmail.com>
2026-07-06 12:47:58 -0400

    Ajuste de actualización

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^m**lkkfhh!e&2i&v1(+uw86d+%47=0fqqutuf@bvv=qi!%-&z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']  # Esto permite que el sistema funcione en la URL que te dé Render
 
 
