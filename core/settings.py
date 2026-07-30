05db6ca5a598a0f247c76c35ace252c1a0fe3a14
Jose Ignacio Lima Subieta <joseignaciolimasubieta@MacBook-Pro-de-Jose-Ignacio.local>
2026-07-29 21:58:27 -0400

    Ajuste de actualización

--- core/settings.py
+++ core/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from dotenv import load_dotenv 
-
-load_dotenv()
+try:
+    from dotenv import load_dotenv 
+    load_dotenv()
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
